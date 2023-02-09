6d9ab580720cf9eddd8ef01cbf60c97268f1d3ee
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-09 07:43:31 +0300

    ✨ Generate Team members component in pages/about-us

--- libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
+++ libs/pages/elewa/about-us/src/lib/elewa-about-us.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TeamMembersComponent } from './team-members/team-members.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [TeamMembersComponent],
+  exports: [TeamMembersComponent]
 })
 export class AboutUsModule {}
---
+++ libs/pages/elewa/about-us/src/lib/team-members/team-members.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-team-members',
+  templateUrl: './team-members.component.html',
+  styleUrls: ['./team-members.component.scss'],
+})
+export class TeamMembersComponent {}
