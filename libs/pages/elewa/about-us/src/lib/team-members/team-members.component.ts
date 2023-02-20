7160271c72dcfeef7f36bda7852d01af27990426
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-20 20:31:37 +0300

    feat: Complete card styling, remaining with slider.

--- libs/pages/elewa/about-us/src/lib/team-members/team-members.component.ts
+++ libs/pages/elewa/about-us/src/lib/team-members/team-members.component.ts
@@ -1,8 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'elewa-group-team-members',
   templateUrl: './team-members.component.html',
   styleUrls: ['./team-members.component.scss'],
 })
-export class TeamMembersComponent {}
+export class TeamMembersComponent implements OnInit{
+
+  @Input () currentIndex = 0
+
+  @Input() teamMembers = [
+    {
+      name: "Jente Rosseel",
+      role: "CEO and Founder",
+      linkedIn: "https://www.linkedin.com/in/jrosseel/",
+      imageUrl: "https://i.postimg.cc/QMgtMpXZ/jente.jpg",
+      summary: ""
+    },
+    {
+      name: "Peter Reinartz",
+      role: "Executive Chairman",
+      linkedIn: "https://www.linkedin.com/in/peter-reinartz-1b4452ab/",
+      imageUrl: "https://i.postimg.cc/K8xcH1P9/peter.png",
+      summary: ""
+    },
+    {
+      name: "Kennedy Adhola",
+      role: "Operations and Partnerships",
+      linkedIn: "https://www.linkedin.com/in/kennedy-adhola-97570535/",
+      imageUrl: "https://i.postimg.cc/h4LH3p2B/kennedy.png",
+      summary: ""
+    },
+    {
+      name: "Herman Vandenbranden",
+      role: "Management",
+      linkedIn: "https://www.linkedin.com/in/herman-vandenbranden-82b53a2/",
+      imageUrl: "https://i.postimg.cc/9XLBM9M9/herman-vandenbranden.jpg",
+      summary: ""
+    },
+    {
+      name: "Ian Obutho",
+      role: "Senior Developer",
+      linkedIn: "",
+      imageUrl: "https://i.postimg.cc/PfFTM6J5/ian.png",
+      summary: ""
+    },
+  ]
+
+  
+  ngOnInit() {
+    this.currentIndex = 0;
+  }
+
+  next() {
+    if (this.currentIndex < this.teamMembers.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  previous() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+}
--- libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
+++ libs/pages/elewa/home/src/lib/features-elewa-home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
-
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { TeamMembersComponent } from 'libs/pages/elewa/about-us/src/lib/team-members/team-members.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomePageComponent, HomeHeroSectionComponent],
+  declarations: [HomePageComponent, HomeHeroSectionComponent, TeamMembersComponent],
   exports: [HomePageComponent, HomeHeroSectionComponent],
 })
 export class HomePageModule {}
